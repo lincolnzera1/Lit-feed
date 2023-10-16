cf07d72ef4c34050880245bde9ecbd0a44a84dba
lincolnzera1 <guilherme.lincoln.lima.11@gmail.com>
2023-10-16 20:06:06 -0300

    Problemas de rotas vercel

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,6 @@ import "primeflex/primeflex.css";
 import "primeicons/primeicons.css";
 import { PrimeReactProvider } from "primereact/api";
 
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <PrimeReactProvider>
     <App />
